e9f365f037ebde31d77f738f3c73b2ceaae18017
Hsiaoming Yang <me@lepture.com>
2014-12-24 16:49:46 +0800

    Exports resolver

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
 exports = module.exports = require('./lib/');
 exports.createHandler = require('./lib/handler');
+exports.resolver = require('./lib/resolver');
 exports.version = require('./package').version;
